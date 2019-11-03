74a90f72714fce1ff4d3080431557330b1c69705
Malin von Matern <malin.vonmatern@valtech.com>
2019-11-03 23:44:59 +0100

    Use switch case in ApodMedia

--- src/components/Apod/ApodMedia.js
+++ src/components/Apod/ApodMedia.js
@@ -3,18 +3,26 @@ import PropTypes from 'prop-types';
 import ApodImage from './ApodImage';
 
 const ApodMedia = ({ media_type, title, url }) => {
-  return (
-    <>
-      { media_type === 'image' ? (
-        <ApodImage
-          url={url}
-          title={title}
-        />
-      ) : (
-        media_type
-      ) }
-    </>
-  );
+  switch(media_type) {
+  case '':
+    // fallthrough
+  case 'image':
+    return (
+      <ApodImage
+        url={url}
+        title={title}
+      />
+    );
+  case 'video':
+    return (
+      <div style={{display: 'block'}}>
+        <p>Support for videos coming soon! :3</p><br />
+        <a href={url}>Meanwhile, visit the video here!</a>
+      </div>
+    );
+  default:
+    return <p>Unsupported media type: {media_type}</p>;
+  }
 };
 
 ApodMedia.defaultProps = {
